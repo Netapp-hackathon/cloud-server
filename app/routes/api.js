a9023df965b2a38e6828679ecced3479843d1292
Shivam Khandelwal <shivamchinu@gmail.com>
2015-12-10 15:48:50 +0530

    Fix derps.

--- app/routes/api.js
+++ app/routes/api.js
@@ -9,17 +9,17 @@ var apiRoutes 	= express.Router();
 
 
 apiRoutes.post('/authenticate', function(req, res) {
-        var usern = req.body.username;
-
+    var username = req.body.username;
 	var client = ldap.createClient({
-		url: 'ldap://vmwaddc06-prd.hq.netapp.com:389'});
+		url: 'ldap://vmwaddc06-prd.hq.netapp.com:389'
+	});
 
-	client.bind(req.body.username+'@netapp.com', req.body.password, function (err) {
+	client.bind(username+'@netapp.com', req.body.password, function (err) {
 		if (err) {
 			res.json({
 				err: {
 					errNo: 1,
-				errMsg: "Authentication failed, user not found!"
+					errMsg: "Authentication failed, user not found!"
 				}   
 			});
 			console.log(err);
@@ -27,11 +27,11 @@ apiRoutes.post('/authenticate', function(req, res) {
 				if(err) {
 					console.log(err.message);
 				} else {
-					console.log('client disconnected');
+					console.log('LDAP client disconnected');
 				}
 			});
 		} else {
-			var token = jwt.sign(usern, config.secret, {
+			var token = jwt.sign(username, config.secret, {
 				expiresIn: 7 * 24 * 60 * 60 // Seconds worth 7 days!
 			});
 
@@ -46,7 +46,7 @@ apiRoutes.post('/authenticate', function(req, res) {
 
 	});
 });
-
+// Safe routes begin here!
 apiRoutes.use(function(req, res, next) {
 	var token = req.body.token || req.query.token || req.headers['x-access-token'];
 	if (token) {
@@ -59,9 +59,7 @@ apiRoutes.use(function(req, res, next) {
 					}
 				})
 			} else {
-				req.decoded = decoded;
-				req.username = decoded.username;
-				//console.log(decoded);
+				req.username = decoded;
 				next();
 			}
 		});
@@ -83,81 +81,112 @@ apiRoutes.get('/validate', function(req, res) {
 	});
 });
 apiRoutes.get('/ops', function(req, res) {
-	User.findOne({
-		username: req.username
-	}, function(err, user) {
-		if (!user) {
+	var ovs_options = { 
+		"method": "GET",
+		"hostname": "10.74.213.25",
+		"port": "1337",
+		"path": "/api/ops",
+		"headers": {
+			"cache-control": "no-cache",
+		}
+	};
+	var reqs = http.request(ovs_options, function (resp) {
+		var respStr = "";
+
+		resp.on("data", function (chunk) {
+			respStr += chunk;
+		});
+		resp.on("end", function () {
+			console.log(respStr + "\n");
+			res.json(JSON.parse(respStr));
+		});
+		resp.on('error', function() {
+			console.log("Error fetching ops from OVS!");
 			res.json({
 				err: {
 					errNo: 1,
-					errMsg: "Something bad happened!"
+					errMsg: "Unable to fetch ops!"
 				}
-			});
-		} else {
-			var ovs_options = { 
-				"method": "GET",
-				"hostname": "10.74.213.25",
-				"port": "1337",
-				"path": "/api/ops",
-				"headers": {
-					"cache-control": "no-cache",
-				}
-			};
-			var reqs = http.request(ovs_options, function (resp) {
-				var respStr = ""; 
-
-				resp.on("data", function (chunk) {
-					respStr += chunk;
-				}); 
-
-				resp.on("end", function () {
-					console.log(respStr + "\n");
-
-					res.json(JSON.parse(respStr));
-				}); 
-			});
-			reqs.end();
-		}
+			})
+		})
 	});
-})
-apiRoutes.get('/ws', function(req, res) {
-	var username = req.username;
-	User.findOne({
-		username: req.username
-	}, function(err, user) {
-		if (!user) {
-			res.json({
-				err: {
-					errNo: 1,
-					errMsg: "No such user!"
-				}
-			});
-		} else {
-			var ovs_options = {
-				"method": "GET",
-				"hostname": "10.74.213.25",
-				"port": "1337",
-				"path": "/api/ws/?username=john",// + TODO: username,
-				"headers": {
-					"cache-control": "no-cache",
-				}
-			};
-			var reqs = http.request(ovs_options, function (resp) {
-				var respStr = "";
-
-				resp.on("data", function (chunk) {
-					respStr += chunk;
-				});
-
-				resp.on("end", function () {
-					res.json(JSON.parse(respStr));
-				});
-			});
-			reqs.end();
-
-		}
-	});
+	reqs.end();
 });
+apiRoutes.get('/ws', function(req, res) {
+	var ovs_options = {
+		"method": "GET",
+		"hostname": "10.74.213.25",
+		"port": "1337",
+		"path": "/api/ws?username=john",// + TODO: req.username,
+		"headers": {
+			"cache-control": "no-cache",
+		}
+	};
+	var reqs = http.request(ovs_options, function (resp) {
+		var respStr = "";
+
+		resp.on("data", function (chunk) {
+			respStr += chunk;
+		});
+
+		resp.on("end", function () {
+			res.json(JSON.parse(respStr));
+		});
+	});
+	reqs.end();
+});
+apiRoutes.get('/vsim', function(req, res) {
+	var ovs_options = {
+		"method": "GET",
+		"hostname": "10.74.213.25",
+		"port": "1337",
+		"path": "/api/vsim?username=john",// + TODO: req.username,
+		"headers": {
+			"cache-control": "no-cache",
+		}
+	};
+	var reqs = http.request(ovs_options, function (resp) {
+		var respStr = "";
+
+		resp.on("data", function (chunk) {
+			respStr += chunk;
+		});
+
+		resp.on("end", function () {
+			res.json(JSON.parse(respStr));
+		});
+	});
+	reqs.end();
+});
+apiRoutes.post('/ops', function(req, res) {
+	delete req.body.token;
+	req.body.username = req.username;
+	var ovs_options = {
+		"method": "POST",
+		"hostname": "10.74.213.25",
+		"port": "1337",
+		"path": "/api/ops",
+		"headers": {
+			"cache-control": "no-cache",
+			"Content-Type": "application/json"
+		}
+	};
+	var reqs = http.request(ovs_options, function (resp) {
+		var respStr = "";
+
+		resp.on("data", function (chunk) {
+			respStr += chunk;
+		});
+
+		resp.on("end", function () {
+			console.log(respStr);
+			res.json(JSON.parse(respStr));
+		});
+	});
+	console.log("Sending op request forward: " + JSON.stringify(req.body));
+	reqs.end(JSON.stringify(req.body));
+})
+
 apiRoutes.get('/users', function(req, res) {
 	User.find({}, function(err, users) {
 		res.json(users);
